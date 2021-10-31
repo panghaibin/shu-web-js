daa90f6413cde6945aa98652afad5f7e1b5bfbe1
panghaibin <panghaibin@shu.edu.cn>
2021-10-31 22:11:43 +0800

    feat: Ver2.0 刷课功能

--- shu-web.user.js
+++ shu-web.user.js
@@ -1,22 +1,36 @@
 // ==UserScript==
-// @name         上海大学网站增强
+// @name         上海大学网站增强 - 刷课助手
 // @namespace    https://github.com/panghaibin/shu-web-js
-// @version      1.0
-// @description  1.将上海大学选课系统学分完成情况页面的原始成绩换算成绩点，标红压线分数 2.标红选课排名页面排名超过额定人数的课程
+// @version      2.0
+// @description  1.二三轮选课自助刷课，解放双手【人人有课刷，抵制卖课狗】 2.选课系统学分完成情况页面的原始成绩换算成绩点，标红压线分数 2.选课排名页面标红排名超过额定人数的课程
 // @author       panghaibin
 // @match        *://xk.autoisp.shu.edu.cn/CourseSelectionStudent/PlanQuery
 // @match        *://xk.autoisp.shu.edu.cn/StudentQuery/QueryEnrollRank
+// @match        *://xk.autoisp.shu.edu.cn/CourseSelectionStudent/FuzzyQuery
 // @grant        none
 // @license      MIT
 // @supportURL   https://github.com/panghaibin/shu-web-js/issues
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
+
+    //--------刷课配置--------
+    // 任意参数留空将不执行
+    // 输入课程号
+    let course_id = ''
+    // 输入对应的教师号
+    let teacher_id = ''
+    // 刷课间隔，默认 8000 ，单位毫秒 一般不需要修改
+    let delay_time = 8000
+
     if (location.pathname === '/CourseSelectionStudent/PlanQuery') {
-        score_conversion(document)
+        score_conversion(document);
     } else if (location.pathname === '/StudentQuery/QueryEnrollRank') {
-        red_rank(document)
+        red_rank(document);
+    } else if (location.pathname === '/CourseSelectionStudent/FuzzyQuery'
+        && course_id.length === 8 && teacher_id.length === 4) {
+        select_course_helper(document);
     }
 
     function score_conversion(document) {
@@ -77,7 +91,7 @@
             let _rank = row.children[6].innerText.split('-');
             let rank = _rank[_rank.length - 1] * 1;
             if (rank > room) {
-                for (let j = 0; j < row.children.length; ++j){
+                for (let j = 0; j < row.children.length; ++j) {
                     elements[i].children[j].style.color = 'red';
                     elements[i].children[j].style.fontWeight = 'bold';
                 }
@@ -85,4 +99,37 @@
         }
     }
 
+    function select_course_helper(document) {
+        document.getElementsByName('CID')[0].defaultValue = course_id;
+        document.getElementsByName('TeachNo')[0].defaultValue = teacher_id;
+        let i = 0;
+        let select_id = setInterval(function () {
+            document.getElementById('QueryAction').click();
+            setTimeout(function () {
+                let tbllist = document.getElementsByClassName('tbllist');
+                let room = tbllist[0].children[0].children[1].children[8].innerText * 1;
+                let num = tbllist[0].children[0].children[1].children[9].innerText * 1;
+                console.log('第' + ++i + '次 课程号' + course_id + ' 教师号' + teacher_id + ' 容量' + room + ' 人数' + num);
+                if (room > num) {
+                    tbllist[0].children[0].children[1].children[0].children[0].checked = true;
+                    document.getElementById('CourseCheckAction').click();
+                    let across_campus = document.getElementById('NotCollegeAction');
+                    if (across_campus != null) {
+                        setTimeout(function () {
+                                across_campus.click();
+                        }, 1000);
+                    }
+                    tbllist = document.getElementsByClassName('tbllist');
+                    let callback_msg = tbllist[1].children[0].children[1].children[5].innerText
+                    if (callback_msg.includes('成功')) {
+                        clearInterval(select_id);
+                    } else {
+                        // 关闭选课失败的窗口
+                        CloseDialog('divOperationResult');
+                    }
+                    console.log(callback_msg);
+                }
+            }, 1000);
+        }, delay_time);
+    }
 })();
